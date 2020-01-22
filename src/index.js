0e27c84d4b80315761668aedf1d9946780c9eb89
Rodrigo Rampazzo <rtedeschi.dextra@vstsalelo.onmicrosoft.com>
2020-01-21 22:57:46 -0300

    Preparando ambiente simples e conhecido

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './CriadosAutomaticamente/index.css';
+// import App from './CriadosAutomaticamente/App';
+import * as serviceWorker from './CriadosAutomaticamente/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Exercicios from './Exercicios/Exercicios'
+
+ReactDOM.render(<Exercicios />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
