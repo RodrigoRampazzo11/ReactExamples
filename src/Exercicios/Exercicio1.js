b1c07306e02b469d452d26726f7316fdc348aac7
Rodrigo Rampazzo <rtedeschi.dextra@vstsalelo.onmicrosoft.com>
2020-01-21 23:14:06 -0300

    Exercicio 1 - Criando Componente

---
+++ src/Exercicios/Exercicio1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Exercicio1 extends React.Component {
+    render () {
+        return (
+            <p>Este é o Exercício 1!</p>
+        )
+    }
+}
+
+export default Exercicio1
--- src/Exercicios/Exercicios.js
+++ src/Exercicios/Exercicios.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import Exercicio1 from './Exercicio1'
+
 class Exercicios extends React.Component {
     render () {
-        return (
-            <h1>Hello World!</h1>
-        )
+        return (<>
+            <h1>Exercícios</h1>
+
+            <br />
+            <h2>Exercício 1:</h2>
+            <h3>Chamando componente</h3>
+            <Exercicio1 />
+        </>)
     }
 }
 
